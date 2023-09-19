48b5fe35f423c182217e1379f449c2e294870008
Willy <willykipchumba22@gmail.com>
2023-09-19 20:33:16 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+}let result
+  if (distance <= 400){
+result = `This one is on me!`;
+  }else if(distance > 2000 && distance < 2500){result ='I will gladly take your thirty bucks.';
 
+ }else if (distance > 2500){
+result = `No can do.`
+}return result
+  
 function ternaryCheckCity(){
   // Write your code here!
-}
+  return city = "NYC" ? "Ok , sounds good" :"No go"
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+  switch (tip) {
+    case "generous"
+    return "thank you so much"
+    case "not generous"
+    return "Thank you"
+    default "Thanks for everything"
+    return "Byee"
+
+  }
+  //at the end, if I want my function to return something, I need to state it:
+  
